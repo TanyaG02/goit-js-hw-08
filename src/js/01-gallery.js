9dc208fa3f8c8f2fea0a2a4c2471ea398bcaf685
TanyaG02 <tanyagryanko@gmail.com>
2022-10-02 22:23:14 +0300

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let items = galleryItems;
+const gallery = document.querySelector('.gallery');
+const render = items
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', render);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionSelector: 'img',
+  captionDelay: 250,
+});
