a1dfaec836a40c0e5bf1ea112e4b62c848b0269d
TanyaG02 <tanyagryanko@gmail.com>
2022-10-03 01:00:27 +0300

    new commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onFormData, 500));
+
+const formData = {};
+
+function onFormData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
