a3c8ca50e76a1a84e225d9e323d580f0b7575e3c
TanyaG02 <tanyagryanko@gmail.com>
2022-10-03 23:22:07 +0300

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,4 +8,5 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
